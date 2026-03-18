1ee997b90178600c119521653b589649a85624d2
Ukrakquen <mmazar@miumg.edu.gt>
2026-03-18 08:08:48 -0600

    Add files via upload

---
+++ Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Peliculas.Api.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+
+        [Required, StringLength(120)]
+
+        public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required, StringLength(80)]
+
+        public string UsuarioLogin { get; set; } = string.Empty;
+
+        [Required]
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [StringLength(20)]
+        public string Rol { get; set; } = "Usuario";
+    }
+}
